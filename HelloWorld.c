3fbdd4d7285a032c1249817cc7f110b4b7567e36
Jai S. Mankoo <jsmankoo@outlook.com>
2015-11-17 21:25:33 -0500

    added a few comments

--- HelloWorld.c
+++ HelloWorld.c
@@ -4,6 +4,7 @@
 int main(){
     #pragma omp parallel
     {
+        // ranges from 0 to OMP_THREAD_NUM-1 (4 in this case) env variable
         int ID = omp_get_thread_num();
         printf("Hello(%d)", ID);
         printf(" World(%d)\n", ID);
